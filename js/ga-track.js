bac8f7404516b6db41296667a6ed9cc1d7a7a590
unknown <aaron.sykes@mobile55.modea.com>
2012-05-15 14:31:39 -0400

    another try at ga-track.js

--- js/ga-track.js
+++ js/ga-track.js
@@ -13,21 +13,3 @@ var _gaq = _gaq || [];
 	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 	});
-
-$(function() {
-	$('div#Enter').submit(function (e){
-		var search_form = $(this);
-		var form_values = search_form.serializeObject();
-		
-		var form_values_google = {
-			zip_entry    : form_values.zip,
-			radius_entry : form_values.radius,
-			product_entry: form_values.product_group
-		};
-		
-		var form_values_string = JSON.stringify(form_values_google);
-
-		_gaq.push(['_trackEvent', 'store-finder', 'submit', form_values_string]);
-		return true;
-	});
-});
