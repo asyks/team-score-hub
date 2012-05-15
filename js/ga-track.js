fadf536a320dfc1a7f34692189cfb5dc04dc9692
unknown <aaron.sykes@mobile55.modea.com>
2012-05-15 14:44:43 -0400

    added some jquery to ga-track.js for a test

--- js/ga-track.js
+++ js/ga-track.js
@@ -12,4 +12,23 @@
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
 
+//
+//
+//
+//
+$('#search-form').submit(function (e){
+	var search_form = $(this);
+	var form_values = search_form.serializeObject();
+	
+	var form_values_google = {
+		zip_entry    : form_values.zip,
+		radius_entry : form_values.radius,
+		product_entry: form_values.product_group
+	};
+	
+	var form_values_string = JSON.stringify(form_values_google);
+
+	_gaq.push(['_trackEvent', 'store-finder', 'submit', form_values_string]);
+	return true;
+});
 
