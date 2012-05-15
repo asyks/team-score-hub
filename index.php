7d0c1c71595a43d08e82be4df88c8d9b4a6a98db
unknown <aaron.sykes@mobile55.modea.com>
2012-05-15 13:45:50 -0400

    try # 2 at ga-track.js initial

--- index.php
+++ index.php
@@ -24,33 +24,6 @@
 		})();
 	</script>
 
-	<!-- External links and mailto: -->
-	<script type="text/javascript">
-		$(document).ready(function(){
-			var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-			$.getScript(gaJsHost + "google-analytics.com/ga.js", 
-				function(){
-					try { var pageTracker = _gat._getTracker("UA-272061-2");
-        				pageTracker._trackPageview();
-					} catch(err) {}
-					var filetypes = /\.(zip|exe|pdf|doc*|xls*|ppt*|mp3)$/i;
-					$('a').each(function(){
-    							var href = $(this).attr('href');
-							if ((href.match(/^https?\:/i)) && (!href.match(document.domain)))
-								{$(this).click(function() {
-										var extLink = href.replace(/^https?\:\/\//i, '');
-										pageTracker._trackEvent('External', 'Click', extLink);
-								});}
-							else if (href.match(/^mailto\:/i))
-								{$(this).click(function() {
-        									var mailLink = href.replace(/^mailto\:/i, '');
-										pageTracker._trackEvent('Email', 'Click', mailLink);
-								});}
-        				});
-				});
-		});
-	</script>
-
 </head>
 
 <body bgcolor="#CCCCCC">
--- js/ga-track.js
+++ js/ga-track.js
@@ -2,18 +2,16 @@
 //
 //Basic Google Analytics Tracking Code
 //
-$(document).ready(function() {
 var _gaq = _gaq || [];
 	_gaq.push(['_setAccount', 'UA-272061-9']);
 	_gaq.push(['_trackPageview']);
 	_gaq.push(['_trackPageLoadTime']);
 
-(function() {
+$(function() {
 	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	})();
-});
+	});
 
 $(function() {
 	$('div#Enter').submit(function (e){
