812fe5150a30d9327dd79b5a1ef212c5a68532c8
unknown <aaron.sykes@mobile55.modea.com>
2012-05-17 17:53:37 -0400

    another change to ga-track.js and survey2.html

--- js/ga-track.js
+++ js/ga-track.js
@@ -18,13 +18,6 @@ function gaFormEvent(){
 
 	var search_form = $("input:checked");
 	var form_values = search_form.serializeArray();
-/*	
-	var form_values_google = {
-		zip_entry    : form_values.zip,
-		radius_entry : form_values.radius,
-		product_entry: form_values.product_group
-	};
-*/	
 	var form_values_string = JSON.stringify(form_values);
 
 	_gaq.push(['_trackEvent', 'team-score', 'enter', form_values_string]);
