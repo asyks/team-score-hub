1caa9ebbb98a6025d1ec4971837f3705b13d1aa5
unknown <aaron.sykes@mobile55.modea.com>
2012-05-17 15:44:10 -0400

    change to ga-track.js to capture name and value pairs from input checkboxes

--- js/ga-track.js
+++ js/ga-track.js
@@ -15,19 +15,20 @@
 //JQuery for GA event tracking
 //
 function gaFormEvent(){
-	_gaq.push(['_trackEvent', 'team-score', 'enter', 'test-value']);
-	return true;
-/*
+
 	var search_form = $("input:checked");
 	var form_values = search_form.serializeObject();
-	
+/*	
 	var form_values_google = {
 		zip_entry    : form_values.zip,
 		radius_entry : form_values.radius,
 		product_entry: form_values.product_group
 	};
-	
+*/	
 	var form_values_string = JSON.stringify(form_values);
-*/
+
+	_gaq.push(['_trackEvent', 'team-score', 'enter', form_values_string]);
+	return true;
+
 };
 
