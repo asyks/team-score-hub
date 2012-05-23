788dc1858ba80975e01ca57e8d17bf4a265055cb
aaron.sykes <aaronpaulsykes@gmail.com>
2012-05-23 16:46:47 -0400

    change back

--- js/ts-jquery1.js
+++ js/ts-jquery1.js
@@ -4,7 +4,7 @@
 //
 function countChecked() {
 	$('#surveys2').empty();
-	var iframetag = '<iframe src="https://docs.google.com/a/modea.com/spreadsheet/embeddedform?formkey=dGpYYXpUVjhCUzFlN0tKV2FOOUFhRkE6MQ width="800" height="1200" frameborder="0" marginheight="0" marginwidth="0" ">Loading...</iframe>';
+	var iframetag = '<iframe src="https://docs.google.com/a/modea.com/spreadsheet/embeddedform?formkey=dGpYYXpUVjhCUzFlN0tKV2FOOUFhRkE6MQ" width="760" height="1122" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>';
 	var n = $("input:checked").length;
 	for(var x = 0; x < n; x++) { $("#surveys2").append(iframetag);}
 	}
