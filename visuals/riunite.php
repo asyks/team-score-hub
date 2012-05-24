5a0b150d102aac3480687dc9ddad84b06b181ecc
aaron.sykes <aaronpaulsykes@gmail.com>
2012-05-24 15:10:17 -0400

    fixed modea log image link

--- visuals/riunite.php
+++ visuals/riunite.php
@@ -113,7 +113,7 @@
 
 <div id="project_images" style="height:160px">
 
-<img src="/images/modea_logo.png" style="position:absolute;" href="seo.modeaondemand.com/"/>
+<a href="seo.modeaondemand.com/"><img src="/images/modea_logo.png" style="position:absolute;"/></a>
 
 <img src="/images/riunite.jpg" style="position: relative; margin-left: 230px; bottom:50px">
 
