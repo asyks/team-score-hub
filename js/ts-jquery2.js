f51c3fffc1796f9ef155e7585f1e0b77e9c774e2
aaron.sykes <aaronpaulsykes@gmail.com>
2012-05-31 17:04:07 -0400

    trying a bit of JQuery to capture serializeArray() params

--- js/ts-jquery2.js
+++ js/ts-jquery2.js
@@ -3,7 +3,11 @@
 // and appends the same number of google docs form iframes to the div#surveys
 //
 function countChecked() {
+	
+	var form_values = $('input:checked').serializeArray();
+	var param_string = JQuery.param(form_values);
 	$('#surveys2').empty();
 	var n = $("input:checked").length;
+	$('#surveys2').text(param_string);
 	for(var x = 0; x < n; x++) { $('#surveys2').append('<iframe src="https://docs.google.com/a/modea.com/spreadsheet/embeddedform?formkey=dGpYYXpUVjhCUzFlN0tKV2FOOUFhRkE6MQ&entry_51=Chiquita" width="760" height="1122" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>"0">Loading...</iframe>');}
 	}
