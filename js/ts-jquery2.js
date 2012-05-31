0d70c21fa90b6822e1e7ff0ab7ad984290b3c01b
aaron.sykes <aaronpaulsykes@gmail.com>
2012-05-31 16:31:35 -0400

    this one might do it

--- js/ts-jquery2.js
+++ js/ts-jquery2.js
@@ -5,5 +5,5 @@
 function countChecked() {
 	$('#surveys2').empty();
 	var n = $("input:checked").length;
-	for(var x = 0; x < n; x++) { $('#surveys2').append('<iframe src="https://docs.google.com/a/modea.com/spreadsheet/embeddedform?formkey=dGpYYXpUVjhCUzFlN0tKV2FOOUFhRkE6MQ&entry_0=Chiquita" width="760" height="1122" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>"0">Loading...</iframe>');}
+	for(var x = 0; x < n; x++) { $('#surveys2').append('<iframe src="https://docs.google.com/a/modea.com/spreadsheet/embeddedform?formkey=dGpYYXpUVjhCUzFlN0tKV2FOOUFhRkE6MQ&entry_51=Chiquita" width="760" height="1122" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>"0">Loading...</iframe>');}
 	}
