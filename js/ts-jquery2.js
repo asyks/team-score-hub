f5cbe2e2a38e413775811dd6bbbf9d7fbef80321
aaron.sykes <aaronpaulsykes@gmail.com>
2012-06-01 13:29:55 -0400

    maybe $.replaceall() will work where $.replace() failed

--- js/ts-jquery2.js
+++ js/ts-jquery2.js
@@ -5,7 +5,7 @@
 function countChecked() {
 	
 	var form_values = $('input:checked').serializeArray();
-	var param_string = $.param(form_values).replace('=on', '');
+	var param_string = $.param(form_values).replaceall('=on', '');
 	$('#surveys2').empty();
 	var n = $("input:checked").length;
 	$('#surveys2').text(param_string);
