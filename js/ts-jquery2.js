df540732eb54b442b755674898ca38818915630c
aaron.sykes <aaronpaulsykes@gmail.com>
2012-06-01 16:10:51 -0400

    another attempt at printing a list in Jquery and small change to Google Docs form iframe

--- js/ts-jquery2.js
+++ js/ts-jquery2.js
@@ -8,9 +8,9 @@ function countChecked() {
 	var regex = new RegExp('=on','g');
 	var param_string = $.param(form_values).replace(regex, '');
 	var projects = param_string.split('&');
-	$('#surveys2').text(projects);
+	$('#surveys2').text(JSON.stringify(projects));
 
 	$('#surveys2').empty();
 	var n = $("input:checked").length;
-	for(var x = 0; x < n; x++) { $('#surveys2').append('<iframe src="https://docs.google.com/a/modea.com/spreadsheet/embeddedform?formkey=dGpYYXpUVjhCUzFlN0tKV2FOOUFhRkE6MQ&entry_51=Chiquita" width="760" height="1122" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>"0">Loading...</iframe>');}
+	for(var x = 0; x < n; x++) { $('#surveys2').append('<iframe src="https://docs.google.com/a/modea.com/spreadsheet/embeddedform?formkey=dGpYYXpUVjhCUzFlN0tKV2FOOUFhRkE6MQ&entry_51=Chiquita" width="760" height="1122" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>"0">');}
 	}
