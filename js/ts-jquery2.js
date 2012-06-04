373df89294cc824d3dbc00847f9b2f7738fea1ce
aaron.sykes <aaronpaulsykes@gmail.com>
2012-06-04 15:25:25 -0400

    another try at appending list slices with JS

--- js/ts-jquery2.js
+++ js/ts-jquery2.js
@@ -11,12 +11,12 @@ function countChecked() {
 
 //	$('#surveys2').text(param_string);
 	var projects = param_string.split('&');
-	$('#surveys2').text(projects.join(','));
+//	$('#surveys2').text(projects.join(','));
 
 	var n = $("input:checked").length;
 	for(var x = 0; x < n; x++) 
 	{ 
-		$('#surveys2').text(projects[x]);
+		$('#surveys2').append(projects[x]);
 		$('#surveys2').append('<iframe src="https://docs.google.com/a/modea.com/spreadsheet/embeddedform?formkey=dGpYYXpUVjhCUzFlN0tKV2FOOUFhRkE6MQ&entry_51=Chiquita" width="760" height="1122" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>');
 	}
 }
