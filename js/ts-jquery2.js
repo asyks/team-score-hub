4fc32364a45dfe7b79159a2d4daf143912ddb98b
aaron.sykes <aaronpaulsykes@gmail.com>
2012-06-04 15:47:10 -0400

    cleaning up the JS in ts-jquery2.js a bit

--- js/ts-jquery2.js
+++ js/ts-jquery2.js
@@ -8,15 +8,10 @@ function countChecked() {
 	var regex = new RegExp('=on','g');
 	var param_string = $.param(form_values).replace(regex, '');
 	$('#surveys2').empty();
-
-//	$('#surveys2').text(param_string);
 	var projects = param_string.split('&');
-//	$('#surveys2').text(projects.join(','));
-
 	var n = $("input:checked").length;
 	for(var x = 0; x < n; x++) 
 	{ 
-		$('#surveys2').append(projects[x]);
 		$('#surveys2').append('<iframe src="https://docs.google.com/a/modea.com/spreadsheet/embeddedform?formkey=dGpYYXpUVjhCUzFlN0tKV2FOOUFhRkE6MQ&entry_51=' + projects[x] + '" width="760" height="1122" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>');
 	}
 }
